01735b5d78e230eb93277945c14530996aba5b31
Florian Reiter <florian.reiter@prolehre.tum.de>
2024-02-19 00:36:56 +0100

    Complete demo functionality.

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_demoactivity';
 $plugin->release = '0.1.0';
-$plugin->version = 2024021516;
+$plugin->version = 2024021807;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
